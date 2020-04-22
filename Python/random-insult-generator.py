6c8e3346aef50d2a156de7346fc6c683d983748b
justnat3 <58314490+justnat3@users.noreply.github.com>
2020-04-21 23:58:41 -0500

    Update random-insult-generator.py

--- Python/random-insult-generator.py
+++ Python/random-insult-generator.py
@@ -1,25 +1,8 @@
-# //What does the computer need to know?
-
-# //Random Body Parts
-# let randomBodyParts = ['head', 'arms', 'legs', 'feet', 'toes', 'hips'] 
-# //random Describing words
-# let randomAdjectives = ['smelly', 'cunt bearing', 'medically challanged', 'silly', 'crazy', 'clorophormic wasting']
-# //random words for discribing 
-# let randomWords = ['pumbus', 'manbun', 'cunt', 'penis', 'chode', 'hole', 'whore', 'tweater']
-# //randomize body part
-# let randomBodyPart = randomBodyParts[Math.floor(Math.random()*5)]
-# //randomize adjective
-# let randomAdjective = randomAdjectives[Math.floor(Math.random()*6)]
-# //randomize words
-# let randomWord = randomWords[Math.floor(Math.random()*8)]
-# //output
-# alert('Your ' + randomBodyPart + " looks like a " + randomAdjective + ' ' + randomWord )
 
 import random
+
 randomBodyParts = ['head', 'arms', 'legs', 'feet', 'toes', 'hips', 'fingers']
-
 randomAdjectives = ['smelly', 'cunt', 'medically challanged', 'silly', 'crazy', 'clorophormic wasting']
-
 randomWords = ['pumbus', 'manbun', 'cunt', 'penis', 'chode', 'hole', 'whore', 'tweater']
 
 randomBodyPart = random.choice(randomBodyParts)
@@ -27,5 +10,4 @@ randomAdjective = random.choice(randomAdjectives)
 randomWord = random.choice(randomWords)
 
 print('Your ' + randomBodyPart + " looks like a " + randomAdjective + ' ' + randomWord )
-
 input('hit enter to exit')
