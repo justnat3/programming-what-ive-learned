a51b05ad10a19fe03c9f45b3a0c2958c828a6663
justnat3 <nd2010@reedusa.com>
2020-04-21 20:25:43 -0500

    Update madlibs.py

--- Python/madlibs.py
+++ Python/madlibs.py
@@ -1,10 +1,12 @@
-print('Stories: A New Toy, Day at the Zoo')
+print('\nStories: A New Toy, Day at the Zoo\n')
 
-userInput = input('pick a story:')
+userInput = input('\npick a story:')
 usrChoice = userInput.lower()
 
 if usrChoice == 'a new toy':
-    print('There is a new toy on the market that has everyone saying (exclamation)! It is called the (sound) (adjective) (noun) box and will be in stores in (a Month). the (sound) (adjective) (noun) box is a new gadget that lets you do just about anything! It (verb)s, it (verb)s, it even serves (a Drink)! It is easy to operate and requires no instructions! You can also haveit custom made to be any size you want up to (integer) inches and (color) or glow in the darkwith no extra charge! The original product is pocket-sized and (color) there are (integer) jacks on the product for 6V DC power and for upgrades and add-ons. You can add headphones, (plural Noun) monitors, (plural Noun), and more, and use them all at the same time!')
+    print('\nThere is a new toy on the market that has everyone saying (exclamation)! It is called the (sound) (adjective) (noun) box and will be in stores in (a Month). the (sound) (adjective) (noun) box is a new gadget that lets you do just about anything! It (verb)s, it (verb)s, it even serves (a Drink)! It is easy to operate and requires no instructions! You can also haveit custom made to be any size you want up to (integer) inches and (color) or glow in the darkwith no extra charge! The original product is pocket-sized and (color) there are (integer) jacks on the product for 6V DC power and for upgrades and add-ons. You can add headphones, (plural Noun) monitors, (plural Noun), and more, and use them all at the same time!\n')
+
+    print('')
 
     exclamation = input('Everyone saying ___ !:  ')
     sound = input('Pick a Sound: ')
@@ -24,7 +26,9 @@ if usrChoice == 'a new toy':
     pluralNoun = input('Pick a Plural Noun: ')
     pluralNoun2 = input('Pick another Plural Noun: ')
 
-    print('There is a new toy on the market that has everyone saying ' + exclamation + '! It is called the ' + sound + '' + adj + ' ' + noun + ' box and will be in stores in ' + aMonth + '. the ' + sound2 + ' ' + adj2 + ' ' + noun2 + ' box is a new gadget that lets you do just about anything! It ' + verb + 's, it ' + verb2 +'s, it even serves ' + aDrink + '! It is easy to operate and requires no instructions! You can also have it custom made to be any size you want up to ' + integer + ' inches and ' + color + ' or glow in the dark with no extra charge! The original product is pocket-sized and ' + color2 +  'there are ' + integer2 + ' jacks on the product for 6V DC power and for upgrades and add-ons. You can add headphones, ' + pluralNoun + ', monitors, ' + pluralNoun2 + ', and more, and use them all at the same time!')
+    print('')
+
+    print('\nThere is a new toy on the market that has everyone saying ' + exclamation + '! It is called the ' + sound + ' ' + adj + ' ' + noun + ' box and will be in stores in ' + aMonth + '. the ' + sound2 + ' ' + adj2 + ' ' + noun2 + ' box is a new gadget that lets you do just about anything! It ' + verb + 's, it ' + verb2 +'s, it even serves ' + aDrink + '! It is easy to operate and requires no instructions! You can also have it custom made to be any size you want up to ' + integer + ' inches and ' + color + ' or glow in the dark with no extra charge! The original product is pocket-sized and ' + color2 +  'there are ' + integer2 + ' jacks on the product for 6V DC power and for upgrades and add-ons. You can add headphones, ' + pluralNoun + ', monitors, ' + pluralNoun2 + ', and more, and use them all at the same time!\n')
 
 else:
     print('\nThat is not a story in our cataloge\n')
@@ -33,6 +37,8 @@ if usrChoice == 'day at the zoo':
 
     print('\nToday I went to the zoo. I saw a __adjective__  __noun__ jumping up and down in its tree. He __verbPastTense__  __adverb__ through the large tunnel that led to its __adjective__ __noun__. I got some peanuts and passed them through the cage to a gigantic gray __noun__ towering above my head. Feeding that animal made me hungry. I went to get a __adjective__ scoop of ice cream. It filled my stomach. Afterwards I had to __verb__ __adverb__ to catch our bus. When I got home I __verbPast__ my mom for a __adjective__ day at the zoo.\n' )
 
+    print('')
+    
     adj = input('Pick a adjective: ')
     noun = input('Pick a Noun: ')
     verbPast = input('Pick a verb in the past tense: ')
@@ -46,9 +52,11 @@ if usrChoice == 'day at the zoo':
     verbPast2 = input('Pick another verb: ')
     adj4 = input('Pick another adjective: ')
 
+    print('')
+        
     print('\nToday I went to the zoo. I saw a ' + adj + ' ' + noun + ' jumping up and down in its tree. He ' + verbPast + ' ' + adverb + ' through the large tunnel that led to its ' + adj2 + ' ' + noun2 + '. I got some peanuts and passed them through the cage to a gigantic gray ' + noun3 + ' towering above my head. Feeding that animal made me hungry. I went to get a ' + adj3 + ' scoop of ice cream. It filled my stomach. Afterwards I had to ' + verb + ' ' + adverb2 + ' to catch our bus. When I got home I ' + verbPast2 + ' my mom for a ' + adj4 + ' day at the zoo.\n' )
 
-
+input('press enter to exit script')
 
 
 
